87ca1aebbe0af4d0cf85f44d5904b7a70a3de633
RGBCOMPU <santi2014300@gmail.com>
2024-11-07 19:25:37 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+function handlePurchase() {
+    alert('Gracias, por su compra, nos comunicaremos con usted para realizar el envío');
+  }
+  
+  function handleSubmit() {
+    alert('Entendido, nos comunicaremos contigo para asesorarte de la mejor manera');
+  }
+  
+  function handleInput() {
+    // Aquí puedes agregar lógica adicional si es necesario para manejar la entrada del usuario
+  }
+  
